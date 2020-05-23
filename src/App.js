f7c828336edbafb10de9dbfe4f5e240f0112c00a
mohammed afif ahmed <afifahmed456123@gmail.com>
2020-05-24 01:19:52 +0530

    Searching Movies and displaying

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import Main from './Main';
 
 const App = () => {
   return (
     <div className="App">
-
+      <Main/>
     </div>
   );
 }
---
+++ src/Main.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import SearchMovies from "./SearchMovies";
+
+const Main = () => {
+    return(
+        <div className="container">
+            <h1 className="title">React Movie Search</h1>
+            <SearchMovies/>
+        </div>
+    );
+}
+
+export default Main;
